c04b162a1816fc97df20f13292f28d5f84b57e9e
R.F. Schmitt <11559541+ron2015schmitt@users.noreply.github.com>
2022-03-27 18:04:33 -0400

    Create type_sizes.cpp

---
+++ type_sizes.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+ 
+int main()
+{
+
+  printf("sizeof(char) = %d\n", sizeof(char));  
+  printf("sizeof(short) = %d\n", sizeof(short));  
+  printf("sizeof(int) = %d\n", sizeof(int)); 
+  printf("sizeof(long) = %d\n", sizeof(long));  
+  printf("sizeof(off_t) = %d\n", sizeof(off_t)); 
+  printf("sizeof(size_t) = %d\n", sizeof(size_t)); 
+  printf("sizeof(float) = %d\n", sizeof(float)); 
+  printf("sizeof(double) = %d\n", sizeof(double)); 
+
+ 
+  return 0;
+}
+
