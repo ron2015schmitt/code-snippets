a30e32a3f12d666f14e3ea2ff6109daa71565c25
R.F. Schmitt <11559541+ron2015schmitt@users.noreply.github.com>
2022-04-05 21:18:01 -0400

    Create code-snippets.bash

---
+++ code-snippets.bash
@@ -0,0 +1,6 @@
+
+COLOR_GREEN="$(tput setaf 2)"
+COLOR_DEFAULT="$(tput sgr0)"
+
+echo -e "${COLOR_GREEN}All executables ran succesfully${COLOR_DEFAULT}"
+printf "${COLOR_GREEN}All executables ran succesfully${COLOR_DEFAULT}\n"
