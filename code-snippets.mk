5023b7fbb80adb2fe5a097ae2cf171df5b841d52
R.F. Schmitt <11559541+ron2015schmitt@users.noreply.github.com>
2022-04-06 16:44:52 -0400

    Update code-snippets.mk

--- code-snippets.mk
+++ code-snippets.mk
@@ -5,3 +5,12 @@
 EXECUTABLES = $(shell find . -type f -executable)
 cleanexec: FORCE
 	@command rm -f $(EXECUTABLES)
+
+# execute all commands of a recipe in one shell so that variables can be used
+# need make v3.8.2 or later
+.ONESHELL:
+test : FORCE 
+	TEST=hello
+	echo $${TEST}
+	echo $$$$
+	echo $$$$
